c7dedf3e17f359d630d33d8c767af76fb6035e46
George Pickering <29524044+tedjenkins@users.noreply.github.com>
2019-07-31 11:13:46 +0100

    Type declarations for individual projects in types.d.ts

--- _tests/types.d.ts
+++ _tests/types.d.ts
@@ -1,1 +1,22 @@
-declare module '*';
+declare module 'easytask/script.js' {
+  const values: {
+    locStorageKey: string;
+    data: () => object;
+  };
+
+  function submitTask(e: Event): void;
+
+  function amendTask(e: Event, taskId?: string): void;
+
+  function deleteTask(e: Event | object, taskId?: string): void;
+
+  function countCharsInField(e: Event, targ?: EventTarget): void;
+
+  function toggleTaskTitleDesc(e: Event, targ?: EventTarget): void;
+
+  function clearAllTasks(): void;
+
+  function init(): void;
+}
+
+declare module 'emojiweather/script.js';
