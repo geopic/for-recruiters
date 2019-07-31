08bfc6b4604308ec284230c068ac41b41c75bbeb
George Pickering <29524044+tedjenkins@users.noreply.github.com>
2019-07-31 10:36:53 +0100

    'emojiweather': content, layout; API call prep"

--- emojiweather/script.js
+++ emojiweather/script.js
@@ -1,1 +1,23 @@
-$(document).foundation();
+$(document).ready(function() {
+  $(document).foundation();
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: 'https://api.openweathermap.org/data/2.5/forecast',
+      data: {
+        q: $('#city-input').val(),
+        APPID: '2ee8b797967566459f0a37dcd5db6a86'
+      },
+      type: 'GET',
+      dataType: 'json'
+    })
+      .done(function(json) {
+        console.log(json);
+      })
+      .fail(function(xhr, status, err) {
+        console.log('Error!', xhr, status, err);
+      });
+  });
+});
