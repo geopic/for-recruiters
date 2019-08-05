fb0bb2a0cd3c3c981a4d80e76d68ef03ae70884a
George Pickering <29524044+tedjenkins@users.noreply.github.com>
2019-08-05 11:31:48 +0100

    'emojiweather': display weather on successful data retrieval

--- emojiweather/script.js
+++ emojiweather/script.js
@@ -12,6 +12,7 @@ export const handleSubmit = e => {
     url: 'https://api.openweathermap.org/data/2.5/forecast',
     data: {
       q: $('#city-input').val(),
+      units: 'metric',
       APPID: '2ee8b797967566459f0a37dcd5db6a86'
     },
     type: 'GET',
@@ -23,7 +24,40 @@ export const handleSubmit = e => {
 
       // Set up main display
       $.each(json.list, (index, entry) => {
-        console.log(`${index}: ${entry}`);
+        const box = document.createElement('div');
+        box.classList.add('weather-box');
+
+        // Date
+        const dtEl = document.createElement('div');
+        dtEl.classList.add('weather-box-date');
+        dtEl.textContent = moment(entry.dt_txt).format('ddd, hA');
+        box.appendChild(dtEl);
+
+        // Emoji icon
+        const icEl = document.createElement('div');
+        icEl.classList.add('weather-box-icon');
+        icEl.textContent = '<insert emoji here>';
+        box.appendChild(icEl);
+
+        // Temperature
+        const tempEl = document.createElement('div');
+        tempEl.classList.add('weather-box-temp');
+        tempEl.textContent = `${entry.main.temp.toFixed()}°C`;
+        box.appendChild(tempEl);
+
+        // Cloudiness
+        const cldEl = document.createElement('div');
+        cldEl.classList.add('weather-box-cloudiness');
+        cldEl.textContent = `${entry.clouds.all}% cloudy`;
+        box.appendChild(cldEl);
+
+        // Wind
+        const wndEl = document.createElement('div');
+        wndEl.classList.add('weather-box-wind');
+        wndEl.textContent = `Wind: ${entry.wind.speed.toFixed()}mph, ${entry.wind.deg.toFixed()}°`;
+        box.appendChild(wndEl);
+
+        displayEl.append(box);
       });
     })
     .fail((xhr, status, err) => {
