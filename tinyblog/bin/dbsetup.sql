25e043aa1293e6f563b2ad6388bc94ee3fee0ef9
George Pickering <29524044+geopic@users.noreply.github.com>
2019-12-18 10:43:31 +0000

    'tinyblog': fix and improve dbsetup.sql file

--- tinyblog/bin/dbsetup.sql
+++ tinyblog/bin/dbsetup.sql
@@ -3,39 +3,39 @@
  * The database setup file for the 'tinyblog' project in the 'for-recruiters' repository.
  * © George Pickering 2019, https://github.com/geopic
  */
-
 CREATE DATABASE IF NOT EXISTS tinyblog;
-
-CREATE USER 'tinybloguser'@'localhost' IDENTIFIED BY 'password123'; 
-
-GRANT ALL PRIVILEGES ON tinyblog.* TO 'tinybloguser'@'localhost';
-
+CREATE USER 'tinybloguser' @'localhost' IDENTIFIED BY 'password123';
+GRANT ALL PRIVILEGES ON tinyblog.* TO 'tinybloguser' @'localhost';
+FLUSH PRIVILEGES;
 USE tinyblog;
-
+CREATE TABLE `users` (
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` VARCHAR(32) NOT NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `is_deleted` TINYINT(1) NOT NULL DEFAULT 0 -- boolean flag to mark user as deleted. 0 = not deleted, 1 = deleted
+);
 CREATE TABLE `posts` (
-  `id` TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `post_content` TEXT NOT NULL,
-  `post_created` DATETIME NOT NULL
+  `post_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_tags` VARCHAR(128) DEFAULT NULL,
+  -- JSON array of tags for the post stored as string, e.g. "[\'food\', \'cooking\']"
+  `is_deleted` TINYINT(1) NOT NULL DEFAULT 0 -- boolean flag to mark post as deleted. 0 = not deleted, 1 = deleted
 );
-
 CREATE TABLE `comments` (
-  `id` TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `comment_author` VARCHAR(32) NOT NULL,
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `comment_author_id` INT NOT NULL,
   `comment_content` TEXT NOT NULL,
-  `comment_posted` DATETIME NOT NULL,
-  `post_id` TINYINT NOT NULL,
-  CONSTRAINT `fk_post_id`
-    FOREIGN KEY (post_id) REFERENCES posts (id)
-    ON DELETE CASCADE
-    ON UPDATE RESTRICT
+  `comment_posted` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_id` INT NOT NULL,
+  FOREIGN KEY (`comment_author_id`) REFERENCES `users` (`id`),
+  FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
 );
-
 CREATE TABLE `meta` (
-  `id` TINYINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `author_id` INT NOT NULL,
   `author_username` VARCHAR(32) NOT NULL,
   `author_pw` VARCHAR(32) NOT NULL,
   `blog_name` VARCHAR(64) NOT NULL,
-  `blog_created` DATETIME NOT NULL
+  `blog_created` DATETIME NOT NULL,
+  FOREIGN KEY (`author_id`) REFERENCES `users` (`id`)
 );
-
-FLUSH PRIVILEGES;
