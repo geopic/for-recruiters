9f5360808793fac1bd53eb9920b1f50e2c1bc33d
George Pickering <29524044+geopic@users.noreply.github.com>
2019-12-26 13:42:40 +0000

    'tinyblog': save config to .json file, create functions.php file

---
+++ tinyblog/src/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * tinyblog/src/functions.php
+ * The global PHP functions file for the 'tinyblog' project in the 'for-recruiters' repository.
+ * © George Pickering 2019, https://github.com/geopic
+ */
+
+ function findRelativePath(...$segments) {
+   // TODO
+ }
--- tinyblog/src/inc/setupblog.inc.php
+++ tinyblog/src/inc/setupblog.inc.php
@@ -2,19 +2,20 @@
 require_once '../classes/DBConnection.class.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-  header('Location: /');
+  header('Location: '.DIRECTORY_SEPARATOR);
   exit();
-} else {
-  // Config to use for db connection and write to file
-  $config = array('driver' => 'mysql', 'host' => 'localhost', 'dbname' => $_POST['db-name'], 'username' => $_POST['db-user-name'], 'password' => $_POST['db-user-pass']);
-  $confFilePath = '../../bin/settings.config.php';
-
-  $db = new DBConnection($config);
-  
-  // Early return (exit) if db connection fails or config file already exists
-  if ($db->dbc === null || file_exists($confFilePath)) {
-    exit();
-  }
-
-  file_put_contents($confFilePath, $config);
 }
+
+// Config to use for db connection and write to file
+$config = array('driver' => 'mysql', 'host' => 'localhost', 'dbname' => trim($_POST['db-name']), 'username' => trim($_POST['db-user-name']), 'password' => trim($_POST['db-user-pass']));
+$confFilePath = __DIR__.str_replace('/', DIRECTORY_SEPARATOR, '/../../bin/settings.json');
+
+$db = new DBConnection($config);
+
+// Early return (exit) if config file already exists
+if (file_exists($confFilePath)) {
+  echo '<br>Your config file already exists at '.$confFilePath.'. Please delete it and retry the setup process.';
+  exit();
+}
+
+file_put_contents($confFilePath, '{"database": '.json_encode($config).', "info": '. json_encode(array('blogName' => trim($_POST['blog-name']), 'userName' => trim($_POST['user-name']), 'userPass' => trim($_POST['user-pass']))) .'}');
